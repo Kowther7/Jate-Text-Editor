86bc9c1faffd26922e43bf560cc3a086f934e300
Kowther Kahin <kowtherkahin@Kowthers-MacBook-Pro.local>
2023-11-29 17:51:22 -0800

    adds the header text

--- client/src/js/header.js
+++ client/src/js/header.js
@@ -1,5 +1,11 @@
 const header = `
-            JATE TEXT EDITOR       
+/*
+       _____  ____________
+      / /   |/_  __/ ____/
+ __  / / /| | / / / __/   
+/ /_/ / ___ |/ / / /___   
+ ____/_/  |_/_/ /_____/   
+just another text editor
+*/                          
 `;
-
 export { header };
